3c6603c7ad86c791e813e717b21f4a61ee6d6ea2
Ayman Dewan <aymdewan@ucdavis.edu>
2019-10-16 01:02:13 -0700

    done these exercises

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,20 @@
-function isPalindrome(s) {}
+function isPalindrome(s) {
+    //Sanitize the string
+    s = s.toLowerCase().replace(/[\W_]/g, "");
+
+    var left = 0;
+    var right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false;
+        } else {
+            left++;
+            right--;
+        }
+    }
+
+    return true;
+}
 
 module.exports = isPalindrome;
--- exercises/LC5_longestPalindrome/index.js
+++ exercises/LC5_longestPalindrome/index.js
@@ -4,6 +4,31 @@
 // longestPalindrome("abba") --> "abba"
 // longestPalindrome("a") --> "a"
 
-function longestPalindrome(s) {}
+function longestPalindrome(s) {
+    let startIndex = 0;
+    let longestPS = 1;
+
+    function expansion(l, r) {
+        while(l >= 0 && r < s.length && s[l] === s[r]) {
+        let currentLength = r - l + 1;
+
+        if (currentLength > longestPS) {
+            longestPS = currentLength;
+            startIndex = l;
+        }
+            l -= 1;
+            r += 1;
+        }
+    }
+
+    for (var i = 0; i < s.length; i++) {
+        //odd
+        expansion(i-1, i+1);
+        //even
+        expansion(i, i+1);
+    }
+
+    return s.slice(startIndex, startIndex + longestPS);
+}
 
 module.exports = longestPalindrome;
