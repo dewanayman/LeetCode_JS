32eacda4e28166ed2d2535a3dda0d663618c5ded
Ayman Dewan <aymdewan@ucdavis.edu>
2019-10-25 01:27:19 -0700

    done these

--- exercises/LC217_containsDuplicate/index.js
+++ exercises/LC217_containsDuplicate/index.js
@@ -1,3 +1,17 @@
-const containsDuplicate = nums => {};
+const containsDuplicate = nums => {
+    let visited = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+
+        if (visited[num]) {
+            return true;
+        } else {
+            visited[num] = true;
+        }
+    }
+
+    return false;
+};
 
 module.exports = containsDuplicate;
--- exercises/LC21_mergeTwoLists/index.js
+++ exercises/LC21_mergeTwoLists/index.js
@@ -1,3 +1,23 @@
-function mergeTwoLists(l1, l2) {}
+function mergeTwoLists(l1, l2) {
+    let testHead  = {next: null};
+    let current = testHead;
+
+    while (l1 && l2) {
+        if (l1.val < l2.val) {
+            current.next = l1;
+            current = current.next;
+            l1 = l1.next;
+        } else {
+            current.next = l2;
+            current = current.next;
+            l2 = l2.next;
+        }
+    }
+    
+    // rest of other
+    current.next = l1 || l2;
+
+    return testHead.next;
+}
 
 module.exports = mergeTwoLists;
